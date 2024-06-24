ca6a5635094c82aebcc99996227c0967c322cb8f
Dina <dinanath88090@outlook.com>
2024-06-24 13:03:39 +0530

    String

---
+++ StringManupulation/String.php
@@ -0,0 +1,12 @@
+<?php
+$original="Hello, Banku!";
+echo strtolower($original);
+echo "<br>";
+echo strtoupper($original);
+echo "<br>";
+echo strlen($original);
+echo "<br>";
+echo substr($original,7,6);
+echo "<br>";
+
+?>
